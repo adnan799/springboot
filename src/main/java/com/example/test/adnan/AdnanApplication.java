d0c4f667bdce53427639a46ebdef1639a8cc351c
adnan <adnanstein990@gmail.com>
2025-03-05 13:34:00 +0530

    update

---
+++ src/main/java/com/example/test/adnan/AdnanApplication.java
@@ -0,0 +1,13 @@
+package com.example.test.adnan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdnanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdnanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test/adnan/AdnanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test.adnan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdnanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
